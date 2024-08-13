982539008befb8d6e3622bc915bc911cb5361764
Abhishek1017-sh <as20041009@gmail.com>
2024-08-13 23:43:03 +0530

    Add files via upload

---
+++ bubblesort.java
@@ -0,0 +1,35 @@
+/*
+import java.util.Arrays;
+import java.util.Scanner;
+public class bubblesort {
+     static void sortedarr(int[] arr,int idx){
+          boolean flag=false;
+          for(int i=0;i<arr.length-1;i++){
+               for(int j=0;j<arr.length-i-1;j++){
+                    if(arr[j]>arr[j+1]){
+                         int temp=arr[j];
+                         arr[j]=arr[j+1];
+                         arr[j+1]=temp;
+                         flag=true;
+                    }
+               }
+               if(flag==false){
+                    System.out.println(Arrays.toString(arr));
+                    return;
+               }
+          }
+          System.out.println(Arrays.toString(arr));
+     }
+     public static void main(String[] args) {
+          Scanner sc =new Scanner(System.in);
+          System.out.println("enter size of array");
+          int n=sc.nextInt();
+          int[] arr=new int[n];
+          System.out.println("enter elements of array:");
+          for(int i=0;i<n;i++){
+               arr[i]=sc.nextInt();
+          }
+          sortedarr(arr,0);
+     }
+}
+*/
